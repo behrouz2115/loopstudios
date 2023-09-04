b7960a81ddd19869738d19ce80f6123ef655947c
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-09-04 18:38:07 +0900

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      extend: {
+        fontFamily: {
+          sans: ['Josefin Sans', 'sans-serif'],
+          alata: ['Alata'],
+        },
+        letterSpacing: {
+          widest: '.3em',
+        },
+      },
+    },
   },
   plugins: [],
 }
