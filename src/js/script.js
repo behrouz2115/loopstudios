d6ca42ecc8e44c6bb5cf85c5abfdc6ac7656e055
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-09-10 11:42:59 +0900

    done

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,6 @@ btn.addEventListener("click", navToggle);
 
 function navToggle() {
   btn.classList.toggle("open");
-  // menu.classList.toggle("flex");
-  // menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
 };
