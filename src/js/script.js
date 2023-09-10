a5058587f5005ea03fe3f9ca41d22234a3563acd
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2023-09-10 11:28:01 +0900

    hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ const menu = document.getElementById("menu");
 
 btn.addEventListener("click", navToggle);
 
-const navToggle = () => {
+function navToggle() {
   btn.classList.toggle("open");
-  menu.classList.toggle("flex");
-  menu.classList.toggle("hidden");
+  // menu.classList.toggle("flex");
+  // menu.classList.toggle("hidden");
 };
